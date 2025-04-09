3fabaf0f21e4bcee609d1ad01ca6566cc31e51fe
TanPham <xuantan@thuvienphapluat.net>
2025-04-09 15:55:00 +0700

    Sử dụng User Repository

---
+++ EShop.Core/RepositoryContracts/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EShop.Core.Entities;
+
+namespace EShop.Core.RepositoryContracts
+{
+    public interface IUsersRepository
+    {
+        Task<ApplicationUser?> AddUser(ApplicationUser user);
+        Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password);
+    }
+}
---
+++ EShop.Infrastructure/Repositories/UsersRepository.cs
@@ -0,0 +1,29 @@
+﻿
+using EShop.Core.DTO;
+using EShop.Core.Entities;
+using EShop.Core.RepositoryContracts;
+
+namespace EShop.Infrastructure.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        public async Task<ApplicationUser?> AddUser(ApplicationUser user)
+        {
+            user.UserID = Guid.NewGuid();
+
+            return user;
+        }
+
+        public async Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password)
+        {
+            return new ApplicationUser()
+            {
+                UserID = Guid.NewGuid(),
+                Email = email,
+                Password = password,
+                PersonName = "Person name",
+                Gender = GenderOptions.Male.ToString()
+            };
+        }
+    }
+}
