ff006d7951025fd1486b907a6a48b5a7f336ee77
TanPham <xuantan@thuvienphapluat.net>
2025-04-09 14:14:26 +0700

    -Cập nhật Program.cs để thêm services cho infrastructure và core, cấu hình routing, xác thực và phân quyền, và ánh xạ controller routes.
    -Thêm package Microsoft.Extensions.DependencyInjection.Abstractions vào EShop.Core.csproj và EShop.Infrastructure.csproj để hỗ trợ Dependency Injection tốt hơn.

--- EShop.API/Program.cs
+++ EShop.API/Program.cs
@@ -1,6 +1,26 @@
+using EShop.Core;
+using EShop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Infrastructure, Core services to the container.
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+// Add controller to the container.
+builder.Services.AddControllers();
+
+// Build the web application
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Routing
+app.UseRouting();
+
+//Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+//Controller routes
+app.MapControllers();
 
 app.Run();
